78fb2a40a338ee2885fc7054202d8c349467d9cf
ATHARVA YEOLA <114800045+Atha2445@users.noreply.github.com>
2023-01-07 10:25:00 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,14 @@
+# Functionis a bunch of code that is intended to do a particular task
+#  in the whole Python script.
+#  Python used the keyword ‘def’ to define a function.
+
+# Python program to illustrate
+# functions
+def hello():
+    
+    print("....")
+    print("Bye-Bye")    
+
+
+# calling function
+hello()			
